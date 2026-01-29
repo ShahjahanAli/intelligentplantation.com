05bd7f442a399b98a81414ac1e217b2928f6b08c
ParvezFci <parvezrohmanfci@gmail.com>
2026-01-29 12:14:56 +0600

    back to previous

--- README.md
+++
@@ -1,2 +0,0 @@
-# intelligentplantation.com
-This is the Frontend Repo for the AI Based Plant Planning System for Kaz Software
--- resources/js/components/dashboard/AppLogo.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import AppLogoIcon from '@/components/dashboard/AppLogoIcon.vue';
-</script>
-
-<template>
-    <div class="flex items-center justify-center">
-        <AppLogoIcon class="h-15 w-auto object-contain" />
-    </div>
-</template>
